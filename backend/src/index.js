cf46acd5c803f6081ee090c8ee2e9e9b3ceca0eb
Coder7Raj <alwaysraj733@gmail.com>
2026-03-06 15:57:37 +0600

    utils added for api response,error and nandler function

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,18 @@
 import dotenv from "dotenv";
+import app from "./app.js";
 import connectDB from "./db/index.js";
 
+const port = process.env.PORT || 5000;
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("DB connection error: ", err);
+  });
---
+++ backend/src/utils/asyncHandler.js
@@ -0,0 +1,17 @@
+// using promise
+// export const asyncHandler = (requestHandler) => {
+//   (req, res, next) => {
+//     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+//   };
+// };
+
+// using try-catch
+export const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res
+      .status(err.code || 500)
+      .json({ success: false, message: error.message });
+  }
+};
